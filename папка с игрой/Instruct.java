2e49d55767b637abd000c16ddd02464ff4a61fb6
SashaBondareva <49449771+SashaBondareva@users.noreply.github.com>
2019-04-15 20:27:05 +0400

    Revert "Revert "SashaBondareva.github.io""
    This reverts commit ab9413a8c3beb73fac169662737a19c959604abc.

---
+++ папка с игрой/Instruct.java
@@ -0,0 +1,12 @@
+package com.example.hanoitower;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Instruct extends Activity {
+    //метод, отображающий layout с инструкциями
+    public void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.instruct);
+    }
+}
---
+++ папка с игрой/Play.java
@@ -0,0 +1,52 @@
+package com.example.hanoitower;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import java.math.BigDecimal;
+
+public class Play extends Activity {
+    private int totalMoves, minPossibleMoves;//сделанные перемещения; миннимально возможное колличество пермещений
+
+    public void onCreate(Bundle b){
+        super.onCreate(b);
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        setContentView(new Draw(this, displayMetrics.widthPixels, displayMetrics.heightPixels, getIntent().getExtras().getInt("numofdisks")));
+
+
+        // Минимальное колличество перемещений (2^n - 1); n колличество дисков
+        minPossibleMoves = new BigDecimal(2).pow(
+                getIntent().getExtras().getInt("numofdisks")).intValue() - 1;
+    }
+
+    //метод, сообщающий о завершении игры
+    public void gameOver(int moves) {
+        totalMoves = moves;
+        showDialog();
+    }
+
+    public void showDialog() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Игра окончена");
+
+        if (totalMoves > minPossibleMoves)
+            alert.setMessage("Минимально возможное колличкство перемещений " + minPossibleMoves
+                    + ", вы сделали " + totalMoves + ".");
+        else
+            alert.setMessage("Подравляем, вы выйграли!!!!");
+
+        alert.setPositiveButton("Назад", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                finish();
+            }
+        });
+
+        alert.create().show();
+    }
+}
